b38ad1d32c54d7c3c7c99bd7b7f149a647c5b0ed
iamshiv007 <iamshiv20032003@gmail.com>
2023-12-26 12:57:42 +0530

    using state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./style.css";
 
 class Car extends React.Component {
- 
+  constructor(){
+    super();
+    this.state={color:"gray"}
+  }
   render() {
     return (
       <>
       <h1>My first class component</h1>
-        <h1>This is my car.</h1>
+        <h1>This is my {this.state.color} car.</h1>
       </>
     );
   }
