1f115c6162d65618d83937db917e58472654eb55
iamshiv007 <iamshiv20032003@gmail.com>
2023-12-26 12:58:43 +0530

    using props for pass data

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React from "react";
 import "./style.css";
 
 class Car extends React.Component {
-  constructor(){
-    super();
-    this.state={color:"gray"}
-  }
+  // constructor() {
+  //   super();
+  //   this.state = { color: "gray" };
+  // }
   render() {
     return (
       <>
-      <h1>My first class component</h1>
-        <h1>This is my {this.state.color} car.</h1>
+        <h1>My first class component</h1>
+        <h1>This is my {this.props.color} car.</h1>
       </>
     );
   }
 }
 export default Car;
-
