f6eb9a11fa7938020de72a5c5bd120079edc14f0
iamshiv007 <iamshiv20032003@gmail.com>
2023-12-26 13:19:34 +0530

    using setState to change state value

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React from "react";
 import "./style.css";
 
 class Car extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = { color: "gray" };
-  // }
+  constructor() {
+    super();
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1994,
+    };
+  }
+
+  changeColor = () => {
+    this.setState({ color: "blue" });
+  };
   render() {
     return (
       <>
-        <h1>My first class component</h1>
-        <h1>This is my {this.props.color} car.</h1>
-        <Engine />
+        <h1>My {this.state.brand}</h1>
+        <h3>
+          Its is a {this.state.color} {this.state.model} from {this.state.year}
+        </h3>
+        <button onClick={this.changeColor}>Change Color</button>
       </>
     );
   }
 }
 export default Car;
-
-class Engine extends React.Component {
-  render() {
-    return <h1>Hello i am a Engine</h1>;
-  }
-}
