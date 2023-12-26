de6eb1e4aaef9e15daaf4c354e80c0444b95747b
iamshiv007 <iamshiv20032003@gmail.com>
2023-12-26 13:00:23 +0530

    component under component

--- src/App.js
+++ src/App.js
@@ -11,8 +11,15 @@ class Car extends React.Component {
       <>
         <h1>My first class component</h1>
         <h1>This is my {this.props.color} car.</h1>
+        <Engine />
       </>
     );
   }
 }
 export default Car;
+
+class Engine extends React.Component {
+  render() {
+    return <h1>Hello i am a Engine</h1>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import Car from './App';
+import Car from "./App";
 
-const root = createRoot(document.getElementById('app'));
+const root = createRoot(document.getElementById("app"));
 
 root.render(
   <StrictMode>
